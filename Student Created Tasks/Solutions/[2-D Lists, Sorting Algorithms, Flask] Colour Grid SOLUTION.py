fa7037f9f96dc95fa94c26a39115036e8999b45e
Low Wei Feng <158588461+monitorseer@users.noreply.github.com>
2024-10-31 22:44:29 +0800

    Update [2-D Lists, Sorting Algorithms, Flask] Colour Grid SOLUTION.py

--- Student Created Tasks/Solutions/[2-D Lists, Sorting Algorithms, Flask] Colour Grid SOLUTION.py
+++ Student Created Tasks/Solutions/[2-D Lists, Sorting Algorithms, Flask] Colour Grid SOLUTION.py
@@ -50,3 +50,38 @@ formatted_hex_grid = []
 for row in hex_grid:
     formatted_row = '#' + ''.join(row)
     formatted_hex_grid.append(formatted_row)
+
+# Task 1.3
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+# Apply merge sort to formatted_hex_grid
+merge_sort(formatted_hex_grid)
+print(formatted_hex_grid)
